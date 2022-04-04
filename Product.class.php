dd857764d212e6803293c2a299e4aaa72ff358d6
dbudolfson <dylan.budolfson@gmail.com>
2022-04-04 09:50:15 -0500

    Update Product.class.php

--- Product.class.php
+++ Product.class.php
@@ -2,12 +2,20 @@
 
 class Product
 {
-	public int $price,
+	public float $price,
 	public string $name,
 	public string $code,
+	
+	// values only used within class methods
+	protected array $cart();
+	protected float $deliveryFee;
 
-	protected int $deliveryFee;
 
+	public $flowers = [
+		('Red Flower', 'RF1', 32.95),
+		('Green Flower', 'GF1', 22.95),
+		('Blue Flower', 'BF1', 72.95)
+	]
 
 	private function determineDeliveryFee($orderTotal){
 		case $orderTotal < 50:
@@ -19,5 +27,43 @@ class Product
 		default:
 			$fee = 0.00;
 	}
-	
+
+	private function determineDeal(){
+		$redFlower = self::flowers[1];
+		var count = 0;
+		$cartCount = count($this->cart);
+		for($i = 0; $i < $cartCount; $i++){
+			//if there are 2 red flowers in the array, one of them is half price
+			if($array[$i] == $redFlower){
+				//for every second flower, half the price
+				if($i % 2 == 0){
+					$this->cart[$i]->price = number_format($this->cart[$i]->price / 2, 2, '.', '') ;
+				}
+			}
+		}
+		return $cart;
+	}
+
+	public function addToCart($code){
+		$flowers = self::flowers;
+		foreach($flowers as $flower){
+			if($code == $flower[2]){
+				$cart[] = $flower; 
+			}
+		}
+	}
+
+	public function evaluateCart(){
+		// evaluate for deals
+		$this->cart = self::determineDeal($this->cart);
+		// assess delivery fee
+		$this->deliveryFee = self::determineDeliveryFee;
+
+		$total = 0.00;
+		foreach($this->cart as $item){
+			$total += $item[2];
+		}
+		return $total;
+	}
+
 }
