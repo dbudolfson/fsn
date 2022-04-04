c07efb0266d86f3e97f4265b121f188003cfb697
dbudolfson <dylan.budolfson@gmail.com>
2022-04-04 11:48:24 -0500

    Update Product.class.php

--- Product.class.php
+++ Product.class.php
@@ -2,9 +2,6 @@
 
 class Product
 {
-	public float $price;
-	public string $name;
-	public string $code;
 	public array $cart;
 	
 	// values only used within class methods
@@ -16,86 +13,85 @@ class Product
 		['Blue Flower', 'BF1', 72.95]
 	];
 		
-	
-	public function emptyCart(){
-		$this->cart = array();
+	public function __construct(){
+		
 	}
 
-	private function determineDeliveryFee($orderTotal){
-		switch($orderTotal){
-			case $orderTotal < 50:
-			$fee = 4.95;
-			break;
-			case $orderTotal < 90:
-				$fee = 2.95;
-				break;
-			default:
-				$fee = 0.00;
-		}
-		return $fee;		
-	}
-
-	private function determineRedFlowerDeal($cart){
-		$redFlower = 'Red Flower';//self::FLOWERS[0][0];
-		$count = 0;
-		$cartCount = count($cart);
-		for($i = 1; $i < $cartCount; $i++){
-			//if there are 2 red flowers in the array, one of them is half price
-			if($cart[$i][0] == $redFlower){
-				//for every second flower, half the price
-				if(($i +1) % 2 == 0){
-					$cart[$i][2] = number_format($cart[$i][2] / 2, 2, '.', '') ;
-				}
-			}
-		}
-		return $cart;
-	}
 
 	function addToCart($codes){
+		if(!is_array($codes)){
+			$codes = array($codes);
+		}
 		$cart = array();
 		$flowers = Product::FLOWERS;
 		foreach($codes as $code){
 			foreach($flowers as $flower){
 				if($code == $flower[1]){
-					$cart[] = $flower; 
+					$this->cart[] = [
+						"name" => $flower[0],
+						"code" => $code,
+						'price' => $flower[2]
+					]; //$flower; 
 				}
 			}
 		}
-		self::evaluateCart($cart);
+		self::evaluateCart();
 		return $cart;
 	}
 
-function evaluateCart($cart){
+	function evaluateCart(){
 		// evaluate for deals
-		$cart = self::determineRedFlowerDeal($cart);
-		
+		$this->cart = self::determineRedFlowerDeal($this->cart);
+		//var_dump($this->cart);die();
 		$total = 0.00;
-		foreach($cart as $item){
-			$total += $item[2];
-			echo "Item Name: ". $item[0] ." Item Price: ". $item[2] . "\n";
+		foreach($this->cart as $item){
+			$total += $item['price'];
+			echo "Item Name: ". $item['name'] ." Item Price: ". $item['price'] . "\n";
 		}
 		
 		// assess delivery fee
 		$deliveryFee = self::determineDeliveryFee($total);
-		echo "deliveryFee: ". $deliveryFee . "\n";
+		echo "deliveryFee: ". $this->deliveryFee . "\n";
 		$total = number_format($total + $deliveryFee, 2);
 		echo "Total: ". $total . "\n";
 		
-		return $total;
-		$cart = self::emptyCart($cart); // empty the array
+		return $this;
 	}
+
+	private function determineDeliveryFee($orderTotal){
+		switch($orderTotal){
+			case $orderTotal < 50:
+				$this->deliveryFee = 4.95;
+			break;
+			case $orderTotal < 90:
+				$this->deliveryFee = 2.95;
+				break;
+			default:
+				$this->deliveryFee = 0.00;
+		}
+		return $this->deliveryFee;		
+	}
+
+	private function determineRedFlowerDeal(){
+		$redFlower = self::FLOWERS[0][0];
+		$count = 0;
+		$cartCount = count($this->cart);
+		for($i = 1; $i < $cartCount; $i++){
+			//if there are 2 red flowers in the array, one of them is half price
+			if($this->cart[$i][0] == $redFlower){
+				//for every second flower, half the price
+				if(($i +1) % 2 == 0){
+					$this->cart[$i][2] = number_format($this->cart[$i][2] / 2, 2, '.', '') ;
+				}
+			}
+		}
+		return $this->cart;
+	}
+
 
 }
 
-// test cases
-$cart = Product::addToCart(array('BF1', 'GF1'));
-//$total = Product::evaluateCart($cart);
-
-$cart = Product::addToCart(array ('RF1', 'RF1'));
-//$total = Product::evaluateCart($cart);
-
-$cart = Product::addToCart(array ('RF1', 'GF1'));
-//$total = Product::evaluateCart($cart);
-
-$cart = Product::addToCart(array ('BF1', 'BF1', 'RF1', 'RF1', 'RF1' ));
-//$total = Product::evaluateCart($cart);
+// cases
+$x = new Product();
+$x->addToCart('RF1');
+var_dump($x->evaluateCart());
